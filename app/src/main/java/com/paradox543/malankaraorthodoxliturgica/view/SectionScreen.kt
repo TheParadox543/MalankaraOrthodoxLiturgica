44b2843a74a5c362e015dc2f838d1670c7cba0cc
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-05-24 15:26:50 +0530

    format to fit image neatly

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/view/SectionScreen.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/view/SectionScreen.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
@@ -18,9 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.paradox543.malankaraorthodoxliturgica.R
@@ -40,6 +48,8 @@ fun SectionScreen(
     val translations by prayerViewModel.translations.collectAsState()
     val selectedFontSize by prayerViewModel.selectedFontSize.collectAsState()
     val nodes = node.children
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
     prayerViewModel.setTopBarKeys(node.route)
     Scaffold (
         containerColor = Color.Transparent,
@@ -56,43 +66,99 @@ fun SectionScreen(
             Image(
                 painter = painterResource(R.drawable.home),
                 "icon",
-                modifier = Modifier.fillMaxSize()
-            )
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(240.dp),
                 modifier = Modifier
-                    .fillMaxSize()
                     .padding(innerPadding)
-                    .padding(top = 180.dp)
-                    .padding(horizontal = 20.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                itemsIndexed(nodes) { index, node ->
-                    Card(
+                    .requiredWidth(400.dp)
+                    .fillMaxSize(),
+                alignment = Alignment.TopStart,
+                contentScale = ContentScale.Crop
+            )
+            if (screenWidth > 600.dp) {
+                Row {
+                    Spacer(Modifier.padding(horizontal = 160.dp))
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(240.dp),
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                            .clickable {
-                                if (node.children.isNotEmpty()) {
-                                    navController.navigate("section/${node.route}")
-                                } else if (node.filename.isNotEmpty()) {
-                                    navViewModel.setSiblingNodes(nodes)
-                                    navViewModel.setCurrentSiblingIndex(index)
-                                    navController.navigate("prayerScreen/${node.route}")
-                                }
-                            },
-                        shape = RoundedCornerShape(8.dp),
-                        elevation = CardDefaults.cardElevation(4.dp)
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(horizontal = 20.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly
                     ) {
-                        val text = node.route.split("_").last()
-                        Text(
-                            text = translations[text] ?: text,
-                            fontSize = selectedFontSize,
-                            modifier = Modifier.padding(16.dp)
-                        )
+                        itemsIndexed(nodes) { index, node ->
+                            SectionCard(
+                                node,
+                                navController,
+                                navViewModel,
+                                nodes,
+                                index,
+                                translations,
+                                selectedFontSize
+                            )
+                        }
+                    }
+                }
+            }
+            else {
+                Column {
+                    Spacer(Modifier.weight(0.25f))
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(240.dp),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(horizontal = 20.dp)
+                            .weight(0.75f),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        itemsIndexed(nodes) { index, node ->
+                            SectionCard(
+                                node,
+                                navController,
+                                navViewModel,
+                                nodes,
+                                index,
+                                translations,
+                                selectedFontSize
+                            )
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun SectionCard(
+    node: PageNode,
+    navController: NavController,
+    navViewModel: NavViewModel,
+    nodes: List<PageNode>,
+    index: Int,
+    translations: Map<String, String>,
+    selectedFontSize: TextUnit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                if (node.children.isNotEmpty()) {
+                    navController.navigate("section/${node.route}")
+                } else if (node.filename.isNotEmpty()) {
+                    navViewModel.setSiblingNodes(nodes)
+                    navViewModel.setCurrentSiblingIndex(index)
+                    navController.navigate("prayerScreen/${node.route}")
+                }
+            },
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        val text = node.route.split("_").last()
+        Text(
+            text = translations[text] ?: text,
+            fontSize = selectedFontSize,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
