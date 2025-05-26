b2699a9c2059d0534c66fc4fb80c1fa228d79fda
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-05-26 09:56:36 +0530

    added feasts of ascension and pentecost

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.paradox543.malankaraorthodoxliturgica"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.3.1"
+        versionCode = 13
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
