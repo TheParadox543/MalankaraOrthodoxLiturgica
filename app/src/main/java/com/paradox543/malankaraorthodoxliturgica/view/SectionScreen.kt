2901d11bc42a3c1f80e3dca6eb97b3ef26727f90
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-06-13 00:13:25 +0530

    visual changes

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/view/SectionScreen.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/view/SectionScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
