dd349def2b3cbd1cca51d257f57ab6b703710554
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-09-11 12:52:54 +0530

    improve FAB UI

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/qr/QrFabGenerate.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/qr/QrFabGenerate.kt
@@ -1,12 +1,19 @@
 package com.paradox543.malankaraorthodoxliturgica.qr
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,22 +23,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.paradox543.malankaraorthodoxliturgica.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QrFabGenerate(
-    navController: NavController,
-    currentDeepLink: String
+    currentDeepLink: String,
+    modifier: Modifier = Modifier,
 ) {
     var showDialog by remember { mutableStateOf(false) }
     var qrBitmap by remember { mutableStateOf<android.graphics.Bitmap?>(null) }
+    val tooltipState = rememberTooltipState()
 
-    FloatingActionButton(onClick = {
-        qrBitmap = generateQrBitmap(currentDeepLink)
-        showDialog = true
-    }) {
-        Icon(painterResource(R.drawable.qr_code), contentDescription = "Generate QR")
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+        tooltip = {
+            Card {
+                Text(
+                    "Generate QR Code",
+                    Modifier.padding(12.dp),
+                    style = MaterialTheme.typography.labelLarge,
+                )
+            }
+        },
+        state = tooltipState,
+    ) {
+        FloatingActionButton(
+            onClick = {
+                qrBitmap = generateQrBitmap(currentDeepLink)
+                showDialog = true
+            },
+            modifier = modifier,
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+        ) {
+            Icon(painterResource(R.drawable.qr_code), contentDescription = "Generate QR")
+        }
     }
 
     if (showDialog && qrBitmap != null) {
