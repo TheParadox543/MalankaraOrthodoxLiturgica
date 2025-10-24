ed6fb992af4ea702eecc8b0ce5d95943470af916
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-10-24 09:08:00 +0530

    refactor ui components of screen

---
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/components/ErrorBlock.kt
@@ -0,0 +1,24 @@
+package com.paradox543.malankaraorthodoxliturgica.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.paradox543.malankaraorthodoxliturgica.viewmodel.PrayerViewModel
+
+@Composable
+fun ErrorBlock(
+    text: String,
+    prayerViewModel: PrayerViewModel,
+    errorLocation: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleSmall,
+        color = MaterialTheme.colorScheme.error,
+        modifier = modifier.fillMaxWidth(),
+    )
+    prayerViewModel.handlePrayerElementError(text, errorLocation)
+}
