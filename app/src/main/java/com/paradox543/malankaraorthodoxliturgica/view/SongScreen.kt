828c4abb36b2a5f7c782ece7cf5facd6851e8909
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-11-04 19:19:24 +0530

    improved UI for player

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/view/SongScreen.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/view/SongScreen.kt
@@ -2,15 +2,27 @@ package com.paradox543.malankaraorthodoxliturgica.view
 
 import androidx.annotation.OptIn
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,9 +31,13 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -29,6 +45,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
@@ -72,7 +89,7 @@ fun SongScreen(
     }
 
     Scaffold(
-        topBar = { TopNavBar(songFilename.substringAfterLast("/"), navController) },
+        topBar = { TopNavBar(songFilename.substringAfterLast("/").removeSuffix(".mp3"), navController) },
         bottomBar = { BottomNavBar(navController) },
     ) { innerPadding ->
         Column(
@@ -116,7 +133,37 @@ private fun SongPlayerUI(
     title: String,
     sourceMessage: String,
 ) {
-    // 1. Display the song metadata first
+    var isPlaying by remember { mutableStateOf(player.isPlaying) }
+    var currentPosition by remember { mutableLongStateOf(player.currentPosition) }
+    var totalDuration by remember { mutableLongStateOf(player.duration) }
+
+    // Use a DisposableEffect to add and remove a listener to the player.
+    DisposableEffect(player) {
+        val listener =
+            object : Player.Listener {
+                override fun onIsPlayingChanged(isPlayingValue: Boolean) {
+                    super.onIsPlayingChanged(isPlayingValue)
+                    isPlaying = isPlayingValue
+                }
+
+                override fun onEvents(
+                    player: Player,
+                    events: Player.Events,
+                ) {
+                    super.onEvents(player, events)
+                    // Update position and duration whenever player state changes
+                    currentPosition = player.currentPosition
+                    totalDuration = player.duration
+                }
+            }
+
+        player.addListener(listener)
+
+        // When the composable is disposed, remove the listener
+        onDispose {
+            player.removeListener(listener)
+        }
+    }
     Text(
         text = title,
         style = MaterialTheme.typography.headlineMedium,
@@ -130,36 +177,57 @@ private fun SongPlayerUI(
     )
 
     // 2. Use a Box to overlay the player controls on top of the image
-    Box(
-        modifier =
-            Modifier
-                .fillMaxWidth(0.9f)
-                .aspectRatio(1f),
-        contentAlignment = Alignment.BottomCenter, // Align controls to the bottom
+    Column(
+        modifier = Modifier.fillMaxWidth(0.9f),
+        verticalArrangement = Arrangement.Bottom, // Align controls to the bottom
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         // The Image serves as the background of the Box
-        val transparent = android.R.color.transparent
         Image(
             painter = painterResource(id = R.drawable.cheeranachan),
             contentDescription = "Song Artwork",
-            modifier = Modifier.fillMaxSize(), // Make the image fill the entire Box
+            modifier =
+                Modifier
+                    .fillMaxWidth(0.9f)
+                    .aspectRatio(1f),
         )
-
-        // The AndroidView hosts the player controls and is placed on top of the Image
-        AndroidView(
+        Row(
             modifier = Modifier.fillMaxWidth(),
-            factory = { context ->
-                PlayerView(context).apply {
-                    this.player = player
-                    useController = true
-                    controllerHideOnTouch = false
-                    controllerShowTimeoutMs = 0 // Show controls indefinitely
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            // Previous Button
+            IconButton(onClick = { player.seekToPreviousMediaItem() }) {
+                Icon(
+                    Icons.Default.KeyboardArrowLeft,
+                    "Previous Song",
+                    modifier = Modifier.size(48.dp),
+                )
+            }
 
-                    setShowNextButton(false)
-                    setShowPreviousButton(false)
-                    setBackgroundColor(transparent)
+            // Play/Pause Button
+            IconButton(onClick = {
+                if (player.isPlaying) {
+                    player.pause()
+                } else {
+                    player.play()
                 }
-            },
-        )
+            }) {
+                if (isPlaying) {
+                    Icon(painterResource(R.drawable.pause_24px), "Pause Song",)
+                } else {
+                    Icon(Icons.Default.PlayArrow, "Play Song")
+                }
+            }
+
+            // Next Button
+            IconButton(onClick = { player.seekToNextMediaItem() }) {
+                Icon(
+                    Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                    "Next Song",
+                    modifier = Modifier.size(48.dp),
+                )
+            }
+        }
     }
 }
