65de3ee5705c945659be61817f8eb922f7a5af1c
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-11-06 23:17:29 +0530

    getting ready for refactoring

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/components/RestoreTimePicker.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/components/RestoreTimePicker.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlin.math.abs
@@ -183,4 +184,10 @@ private fun TimePickerColumn(
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun RestoreTimePickerPreview() {
+    RestoreTimePicker({ minutes -> }, {}, 30)
 }
