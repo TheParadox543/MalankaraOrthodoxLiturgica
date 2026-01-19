ff131aede9bddd8b617a63a8e9b2880c5966f9d0
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2026-01-19 23:27:51 +0530

    update app version
    Updated the app version for Play Store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 40
-        versionName = "2.0.3-alpha"
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
