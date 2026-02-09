31f376bfdbe4ac417e91a86d8822b4f5c6a5990c
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2026-02-09 21:46:16 +0530

    updated translations and image
    Getting ready for publish of v2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.paradox543.malankaraorthodoxliturgica"
         minSdk = 26
         targetSdk = 36
-        versionCode = 48
-        versionName = "2.1.2-beta.2"
+        versionCode = 49
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
