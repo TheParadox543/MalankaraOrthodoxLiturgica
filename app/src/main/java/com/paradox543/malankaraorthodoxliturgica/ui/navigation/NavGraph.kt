a71d731325c2f22e39c6fcf0610b9452d0680925
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2026-02-16 21:22:55 +0530

    comment out music section

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/navigation/NavGraph.kt
@@ -103,16 +103,16 @@ fun NavGraph(
             }
         }
 
-        composable(
-            route = AppScreen.MusicSection.route,
-            arguments =
-                listOf(
-                    navArgument(AppScreen.MusicSection.ARG_ROUTE) {
-                        type = NavType.StringType
-                    },
-                ),
-        ) { backStackEntry ->
-        }
+//        composable(
+//            route = AppScreen.MusicSection.route,
+//            arguments =
+//                listOf(
+//                    navArgument(AppScreen.MusicSection.ARG_ROUTE) {
+//                        type = NavType.StringType
+//                    },
+//                ),
+//        ) { backStackEntry ->
+//        }
 
         composable(
             route = AppScreen.Prayer.route,
