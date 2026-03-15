531ef0c8b2f74a787c3ed343569dff4288b6eba5
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2026-03-15 11:41:36 +0530

    fix: add missing file, remove obsolete import
    greatLent image was missing, lost during refactor. An import of viewModel was still left in a place no longer required.

--- app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/components/ErrorBlock.kt
+++ app/src/main/java/com/paradox543/malankaraorthodoxliturgica/ui/components/ErrorBlock.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.paradox543.malankaraorthodoxliturgica.ui.viewmodel.PrayerViewModel
 
 @Composable
 fun ErrorBlock(
