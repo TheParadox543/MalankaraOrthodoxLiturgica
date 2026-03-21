0581d3246276649082ef9cf0f27de6c57737c2d6
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2026-03-21 16:27:30 +0530

    chore: update kotlin libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    kotlin("plugin.serialization") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    kotlin("plugin.serialization") version "2.1.21" apply false
     // Google services plugins
     id("com.google.gms.google-services") version "4.4.2" apply false
     // Add the dependency for the Crashlytics Gradle plugin
--- core/domain/src/main/java/com/paradox543/malankaraorthodoxliturgica/domain/bible/model/BookNotFoundException.kt
+++ core/domain/src/main/java/com/paradox543/malankaraorthodoxliturgica/domain/bible/model/BookNotFoundException.kt
@@ -1,7 +1,5 @@
 package com.paradox543.malankaraorthodoxliturgica.domain.bible.model
 
-import java.io.IOException
-
 class BookNotFoundException(
     message: String,
-) : IOException(message)
+) : NoSuchElementException(message)
